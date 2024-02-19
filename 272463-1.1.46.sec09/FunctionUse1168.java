be75c7bed41beab326d64f2662feef81d255e868
duzhg56980 <guokey1996@gmail.com>
2024-02-19 15:17:50 +0800

    init commit

---
+++ 272463-1.1.46.sec09/FunctionUse1168.java
@@ -0,0 +1,7 @@
+import com.alibaba.fastjson.parser.JSONReaderScanner;
+public class FunctionUse1168 {
+public void funcUse() {
+JSONReaderScanner jsonreaderscanner = new JSONReaderScanner();
+jsonreaderscanner.stringVal();
+}
+}
---
+++ 272463-1.1.46.sec09/FunctionUse1354.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.parser.JSONScanner;
+public class FunctionUse1354 {
+public void funcUse() {
+JSONScanner jsonscanner = new JSONScanner();
+}
+}
